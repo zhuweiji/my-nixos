4d2d7fe2b7bc7ee92a837db309ccc62b2893916c
Zhu Weiji <zhuweiji1997@gmail.com>
2024-09-23 08:39:21 +0000

    update

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,14 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     # include NixOS-WSL modules
     <nixos-wsl/modules>
@@ -17,9 +18,9 @@
   wsl.defaultUser = "nixos";
 
   environment.systemPackages = [
+    pkgs.alejandra
     pkgs.git
   ];
-  
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
